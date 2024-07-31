5d9aab60cc7702587504ed1ccea4e11dcd191d2f
Semeniuk Anton <semeniuk.anton.a@gmail.com>
2024-07-31 16:21:31 +0300

    initial commit

---
+++ scripts/hero-banner-slider.js
@@ -0,0 +1,13 @@
+const heroBannerSlider = new Swiper(".hero-banner__slider", {
+	slidesPerView: 1,
+	spaceBetween: 20,
+	initialSlide: document.querySelectorAll(".hero-banner__slider .swiper-slide")
+		.length,
+	pagination: {
+		el: ".hero-banner__slider .swiper-pagination",
+	},
+	navigation: {
+		nextEl: ".hero-banner__navigation-wrapper .swiper-button-next",
+		prevEl: ".hero-banner__navigation-wrapper .swiper-button-prev",
+	},
+});
