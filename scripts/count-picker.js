498ba0706c383053137e33c69404c3519e7ecbb8
Semeniuk Anton <semeniuk.anton.a@gmail.com>
2024-08-15 15:14:30 +0300

    added count picker logic

---
+++ scripts/count-picker.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", () => {
+	const countPickerWrappers = document.querySelectorAll(
+		".count-picker",
+	);
+
+	countPickerWrappers.forEach((countPickerWrapper) => {
+		const countPicker = countPickerWrapper.querySelector('input[type="number"]');
+		const minus = countPickerWrapper.querySelector(".count-picker__minus");
+		const plus = countPickerWrapper.querySelector(".count-picker__plus");
+
+		minus?.addEventListener("click", () => {
+			if (countPicker.value > 1) {
+				countPicker.value -= 1;
+			}
+		});
+		plus?.addEventListener("click", () => {
+			if (countPicker.value < 999) {
+				countPicker.value = +countPicker.value + 1;
+			}
+		});
+
+		countPicker?.addEventListener("input", () => {
+			if (countPicker.value <= 1) {
+				countPicker.value = 1;
+			}
+			if (countPicker.value >= 999) {
+				countPicker.value = 999;
+			}
+		});
+	});
+
+});
