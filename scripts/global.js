32b841441e4fac6c0f38488b726ab8ee8159a476
denisobminianyi <71834229+denisobminianyi@users.noreply.github.com>
2024-11-04 16:10:52 +0200

    fix scroll

--- scripts/global.js
+++ scripts/global.js
@@ -1,5 +1,6 @@
 function toggleScroll(scrollTop) {
     document.body.classList.toggle('remove-scrolling', document.querySelector('.modal--opened') !== null);
+    const isScrollDisabled = document.body.classList.contains('remove-scrolling');
     if (isScrollDisabled) {
         document.body.style.top = `-${scrollTop}px`;
     }
